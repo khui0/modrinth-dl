23a0f45d42737262dee7c96ff6dd94ae02f13e8e
Kenny <101839505+khui0@users.noreply.github.com>
2025-03-04 15:49:59 -0500

    feat: fetch project version

--- src/modrinth.ts
+++ src/modrinth.ts
@@ -31,3 +31,81 @@ export const MODRINTH_VERSIONS: string[] = (await fetchVersions()).map(
 export const MODRINTH_LOADERS: string[] = (await fetchLoaders()).map(
   (item: { name: string }) => item.name
 );
+
+export async function fetchMetadata(
+  target: string,
+  loader: string,
+  slugs: string[]
+) {
+  const metadata: {
+    name?: string;
+    version_number?: string;
+    changelog?: string;
+    dependencies?: unknown[];
+    game_versions?: string[];
+    version_type?: string;
+    loaders?: string[];
+    featured?: boolean;
+    status?: string;
+    requested_status?: string;
+    id: string;
+    project_id: string;
+    author_id: string;
+    date_published: string;
+    downloads: number;
+    changelog_url?: string;
+    files: unknown[];
+  }[] = [];
+  const errors: {
+    slug: string;
+    error: string;
+  }[] = [];
+
+  const promises = slugs.map(async (slug: string) => {
+    const parts = slug.split(":");
+    const versionType = parts[1]?.toLowerCase() || "release";
+
+    const project = await fetchProject(parts[0]);
+    if (!project) {
+      errors.push({ slug, error: `Unable to find ${slug}` });
+    }
+    if (!project.loaders.includes(loader)) {
+      errors.push({
+        slug,
+        error: `${parts[0]} does not support loader: ${loader}`,
+      });
+    }
+    if (!project.game_versions.includes(target)) {
+      errors.push({
+        slug,
+        error: `${parts[0]} does not support version: ${target}`,
+      });
+    }
+
+    const versions = (await fetchProjectVersions(parts[0])).filter(
+      (item: {
+        game_versions: string;
+        loaders: string;
+        version_type: string;
+      }) =>
+        item.game_versions.includes(target) &&
+        item.loaders.includes(loader) &&
+        item.version_type === versionType
+    );
+
+    if (versions.length === 0) {
+      errors.push({
+        slug,
+        error: `Unable to find matching version for ${slug}`,
+      });
+    } else {
+      const selected = versions[0];
+      metadata.push(selected);
+    }
+    return;
+  });
+
+  await Promise.allSettled(promises);
+
+  console.log(metadata);
+}
